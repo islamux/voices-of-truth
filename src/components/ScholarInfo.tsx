68988d0d278bab7de745ec0cac8723b8a8ad36ed
Islamux <fathi733l@gmail.com>
2025-09-24 17:42:03 +0300

    fix: Correct CSS variable typos

---
+++ src/components/ScholarInfo.tsx
@@ -0,0 +1,22 @@
+interface ScholarInfoProps{
+  name: string;
+  country :string;
+  bio: string | null | undefined;
+  languages: string[];
+}
+
+const ScholarInfo: React.FC<ScholarInfoProps> = ({
+  name, country, bio, languages
+}) =>{
+  return (
+    <>
+    <h3
+    className="text-lg sm:text-xl font-semibold text-[rgb(var(--foreground-rgb))] mb-1">{name}</h3>
+    <p className="text-xs sm:text-xl text-[rgb(var(--muted-text-rgb))] mb-2 italic px-2">{country}</p>
+    {bio && <p className="text-xs sm:text-xs text-[rgb(var(--muted-text-rgb))] mb-2 italic px-2">{bio}</p>}
+    <p className="txs sm:text-sm text-[rgb(var(--muted-text-rgb))] mb-3"> languages:{languages.join(', ')} </p>
+    </>
+  );
+};
+
+export default ScholarInfo;
---
+++ src/components/SocialMediaLinks.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaTwitter, FaYoutube, FaFacebook, FaInstagram, FaTelegram, FaLink } from "react-icons/fa";
+import { IconBase, IconContext } from "react-icons";
+import { Scholar } from "@/types";
+import { link } from "fs";
+import { platform } from "os";
+import { defaultHead } from "next/head";
+
+interface SocialMediaLinksProps{
+  socialMedia: Scholar['socialMedia'];
+  name: string;
+}
+
+const SocialMediaLinks: React.FC<SocialMediaLinksProps> = ({
+  socialMedia,name }) => {
+
+    const renderSocialIcon = (platform: string, link: string, icon?: string) => {
+      let iconElement;
+      switch(icon){
+        case 'FaTwitter' : iconElement = <FaTwitter/>; break;
+        case 'FaYoutube' : iconElement = <FaYoutube/>; break;
+        case 'FaFacebook' : iconElement = <FaFacebook/>; break;
+        case 'FaInstagram' : iconElement = <FaInstagram/>; break;
+        case 'FaTelegram' : iconElement = <FaTelegram/>; break;
+        default: iconElement = <FaLink/>;
+      }
+      return (
+        <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-[rgb(var(--muted-text-rgb))] hover:text-[rgb(var(--foreground-rgb))] transition-colors mx-2"
+        aria-label={`${platform} link for ${name}`}
+        key={link}
+      >
+        <IconContext.Provider value={{size: "1.5em"}}>
+        {iconElement}
+        </IconContext.Provider>
+        <span className="sr-only">{platform}</span>
+        </a>
+      );
+    };
+
+
+    return(
+      <div className="mt-auto pt-3 border-t border-[rgb(var(--card-border-rgb))] w-full flex justify-center items-center">
+      {socialMedia.map( (social)=>
+        renderSocialIcon(social.platform, social.link, social.icon)
+      )}
+      </div>
+    );
+  };
+export default SocialMediaLinks;
